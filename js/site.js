fbeb4d66de5fc2d87277f8489250f80beed20f7c
Stephen <sfoster2010.sf@gmail.com>
2023-03-02 14:58:07 -0500

    reverse a string done

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-// Call Hello World function
-function helloWorld() {
-    alert("Hello World");
+//Get the string/value from the page
+//controller function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString) {
+
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+
+//Display our reversed string to the user
+//view function
+function displayString(revString) {
+
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
